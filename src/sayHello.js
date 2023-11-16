3b7e2d93af9d83e87ea7a0d779a2932eace7622e
aliaksandr.krasnou <aliaksandr.krasnou@ehu.lt>
2023-11-16 06:56:54 +0000

    fix: fix sayHello.js

--- src/sayHello.js
+++ src/sayHello.js
@@ -1,8 +1,8 @@
 /**
  * Prints message Hello, {name}! (name = World by default)
- * @param {string} name - to be inserted into final Hello, name! string
+ * @param {string} name - to be inserted into final "Hello, name!"" string
  * @returns {string}
  */
 module.exports.sayHello = function sayHello(name = 'World') {
-  return `Hello, ${name}!`;
+  throw new Error('Not implemented'); // remove this line
 };
