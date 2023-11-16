d6c62fb8287223890cdaef1176d6d5e91d1225d3
aliaksandr.krasnou <aliaksandr.krasnou@ehu.lt>
2023-11-16 11:01:47 +0300

    feat: add tasks-02-test-tasks

---
+++ src/even.js
@@ -0,0 +1,8 @@
+/**
+ * Print "EVEN" or "ODD" if a number is even or odd
+ * @param {number} num - -100000 < n < 100000
+ * @returns {string}
+ */
+module.exports.even = function even(num) {
+  throw new Error('not implemented');
+};
