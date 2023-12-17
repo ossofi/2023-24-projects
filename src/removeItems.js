2779a05c4e07ba89ae6b38bc3be38dbaf6b8dba2
ossofi <osinskaasona@gmail.com>
2023-12-17 23:44:36 +0300

    fix(ESDU-03): fix 2

--- src/removeItems.js
+++ src/removeItems.js
@@ -5,28 +5,6 @@
  * @returns {Object}
  */
 module.exports.removeItems = function removeItems(arr, value) {
-  // const res = arr.filter(elem => elem !== value);
-  // return res;
-  return arr.filter(item => {
-    if (Array.isArray(item)) {
-      return removeItems(item, value).length > 0;
-    } else if (typeof item === 'object' && item !== null) {
-      return removeItemsFromObj(item, value);
-    }
-    return item !== value;
-  });
+  const strValue = JSON.stringify(value);
+  return arr.filter(item => JSON.stringify(item) !== strValue);
 };
-
-function removeItemsFromObj(obj, value) {
-  for (const key in obj) {
-    const item = obj[key];
-    if (Array.isArray(item)) {
-      obj[key] = removeItems(item, value);
-    } else if (typeof item === 'object' && item !== null) {
-      removeItemsFromObj(item, value);
-    } else if (item === value) {
-      delete obj[key];
-    }
-  }
-  return Object.keys(obj).length > 0;
-}
